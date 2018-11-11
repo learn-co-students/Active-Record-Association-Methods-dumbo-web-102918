df93abd867b048e37d7b47fa71c64eaaa21f7e8c
github username <github email address>
2018-11-11 17:27:47 -0500

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,7 +15,7 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
     self.artists.map do |musician|
-      "#{musician.name}"
+      musician.name
   end
 end
 end
